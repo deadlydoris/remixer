5c9d205a53c3874f72c8a88f6b4288e103affaa8
Jonathan Valdez <jnthn.vldzz@gmail.com>
2018-01-21 15:48:28 -0600

    added validation for splice numbers

--- static/index.js
+++ static/index.js
@@ -8,13 +8,17 @@ function mash() {
     if (track_input_2.files.length) {
         music_files.push(track_input_2.files[0]);
     }
-    let splice_length_1 = document.querySelector("#splice_length_1").value
-    let splice_length_2 = document.querySelector("#splice_length_2").value
+    let splice_length_1 = parseFloat(document.querySelector("#splice_length_1").value);
+    let splice_length_2 = parseFloat(document.querySelector("#splice_length_2").value);
     console.log(music_files);
     console.log(splice_length_1);
     console.log(splice_length_2);
+
+    // conditional only going to execute post if we have files - only post data if spl1 and spl2 are set and #s
+
+
  
-    if (music_files.length == 2) {
+    if (music_files.length == 2 && splice_length_1 > 0 && splice_length_2 > 0) {
         var fdata = new FormData()
         fdata.append("music_file_1", music_files[0]);
         fdata.append("music_file_2", music_files[1]);
